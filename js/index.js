833e1a5677d9d09f6128a33f37794f3090e9422e
gilsefti <gilsefti@gmail.com>
2014-10-20 15:01:58 +0300

    foodP

--- js/index.js
+++ js/index.js
@@ -1,84 +1,84 @@
-          <!-- These are the notifications that are displayed to the user through pop-ups if the above JS files does not exist in the same directory-->
+           <!-- These are the notifications that are displayed to the user through pop-ups if the above JS files does not exist in the same directory-->
 
 
-         /*function getSession() {
+          /*function getSession() {
                 alert("session: " + JSON.stringify(FB.getSession()));
             }
             */
-         function getLoginStatus() {
-             FB.getLoginStatus(function (response) {
-                 if (response.status == 'connected') {
-                     alert('logged in');
-                 } else {
-                     alert('not logged in');
-                 }
-             });
-         }
-         var friendIDs = [];
-         var fdata;
+          function getLoginStatus() {
+              FB.getLoginStatus(function (response) {
+                  if (response.status == 'connected') {
+                      alert('logged in');
+                  } else {
+                      alert('not logged in');
+                  }
+              });
+          }
+          var friendIDs = [];
+          var fdata;
 
-         function me() {
-             FB.api('/me', {}, function (response) {
-                 if (response.error) {
-                     alert(JSON.stringify(response.error));
-                 } else {
-                     var data = document.getElementById('data');
-                     fdata = response.data;
-                     console.log("fdata: " + fdata);
-                     response.data.forEach(function (item) {
-                         var d = document.createElement('div');
-                         d.innerHTML = "<img src=" + item.picture + "/>" + item.name;
-                         data.appendChild(d);
-                     });
-                 }
-                 var friends = response.data;
-                 console.log(friends.length);
-                 for (var k = 0; k < friends.length && k < 200; k++) {
-                     var friend = friends[k];
-                     var index = 1;
+          function me() {
+              FB.api('/me', {}, function (response) {
+                  if (response.error) {
+                      alert(JSON.stringify(response.error));
+                  } else {
+                      var data = document.getElementById('data');
+                      fdata = response.data;
+                      console.log("fdata: " + fdata);
+                      response.data.forEach(function (item) {
+                          var d = document.createElement('div');
+                          d.innerHTML = "<img src=" + item.picture + "/>" + item.name;
+                          data.appendChild(d);
+                      });
+                  }
+                  var friends = response.data;
+                  console.log(friends.length);
+                  for (var k = 0; k < friends.length && k < 200; k++) {
+                      var friend = friends[k];
+                      var index = 1;
 
-                     friendIDs[k] = friend.id;
-                     //friendsInfo[k] = friend;
-                 }
-                 console.log("friendId's: " + friendIDs);
-             });
-         }
+                      friendIDs[k] = friend.id;
+                      //friendsInfo[k] = friend;
+                  }
+                  console.log("friendId's: " + friendIDs);
+              });
+          }
 
-         function logout() {
-             FB.logout(function (response) {
-                 alert('logged out');
-             });
-         }
+          function logout() {
+              FB.logout(function (response) {
+                  alert('logged out');
+              });
+          }
 
-         function login() {
-             FB.login(
-                 function (response) {
-                     if (response.status === 'connected') {
-                         alert('logged in');
-                     } else {
-                         alert('not logged in');
-                     }
-                 }, {
-                     scope: ""
-                 }
-             );
-         }
-
-
+          function login() {
+              if (!window.cordova) {
+                  var appId = prompt("Enter FB Application ID", "");
+                  facebookConnectPlugin.browserInit(appId);
+              }
+              facebookConnectPlugin.login(["email"],
+                  function (response) {
+                      alert(JSON.stringify(response))
+                  },
+                  function (response) {
+                      alert(JSON.stringify(response))
+                  });
+          }
 
 
-         document.addEventListener('deviceready', function () {
-             try {
-                 //alert('Device is ready! Make sure you set your app_id below this alert.');
-                 FB.init({
-                     appId: "509210995889450",
-                     xfbml: false,
-                     version: 'v2.1',
-                     nativeInterface: CDV.FB,
-                     useCachedDialogs: false
-                 });
 
-             } catch (e) {
-                 alert(e);
-             }
-         }, false);
+
+          document.addEventListener('deviceready', function () {
+              try {
+                  //alert('Device is ready! Make sure you set your app_id below this alert.');
+                  FB.init({
+                      appId: "509210995889450",
+                      xfbml: false,
+                      version: 'v2.1',
+                      nativeInterface: CDV.FB,
+                      useCachedDialogs: false
+                  });
+
+              } catch (e) {
+                  alert(e);
+              }
+          }, false);
